f338915bdd31631c60c140b7f330a3bd9d440775
Rongjun Wu <rongjun0821@gmail.com>
2021-08-11 21:11:06 +0800

    feat(compiler): step commit

--- src/pages/dashboard/groups.tsx
+++ src/pages/dashboard/groups.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import {PageContainer} from '@ant-design/pro-layout';
-import {Col, Divider, Input, Row, Tree} from 'antd';
-import {DownOutlined} from '@ant-design/icons';
-import {history, Link, useModel} from 'umi';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Col, Divider, Input, Row, Tabs, Tree } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { history, Link, useModel } from 'umi';
 
-const {DirectoryTree} = Tree;
-const {Search} = Input;
+const { DirectoryTree } = Tree;
+const { Search } = Input;
+const { TabPane } = Tabs;
 
 export default (): React.ReactNode => {
   // @ts-ignore
-  const {groups, queryGroup} = useModel('groups', (model) => ({
+  const { groups, queryGroup } = useModel('groups', (model) => ({
     groups: model.groups,
     queryGroup: model.queryGroup,
   }));
   queryGroup();
 
-  const {setInitialState} = useModel('@@initialState');
+  const { setInitialState } = useModel('@@initialState');
   const onSelect = (item) => {
-    console.log(item)
-    setInitialState((s) => ({...s, pathname: history.location.pathname}));
+    console.log(item);
+    setInitialState((s) => ({ ...s, pathname: history.location.pathname }));
   };
 
   const titleRender = (nodeData: any): React.ReactNode => {
-    const {title} = nodeData;
-    // console.log(nodeData)
+    const { title } = nodeData;
     return (
-      <Link to={`/${title}`} style={{color: 'black', lineHeight: '40px'}}>
+      <Link to={`/${title}`} style={{ color: 'black' }}>
         {title}
       </Link>
     );
@@ -34,34 +34,34 @@ export default (): React.ReactNode => {
   return (
     <div>
       <Row>
-        <Col span={4}/>
+        <Col span={4} />
         <Col span={16}>
           <PageContainer>
-            <Divider/>
-            <Search style={{marginBottom: 15}} placeholder="Search"/>
-            {
-              groups.map((item: { title: string, key: string, children?: [] }) => {
-                  const tmp = (
+            <Divider />
+            <Search style={{ marginBottom: 15 }} placeholder="Search" />
+            <Tabs defaultActiveKey="1">
+              <TabPane tab="Your groups" key="1">
+                {groups.map((item: { title: string; key: string; children?: [] }) => {
+                  const hasChildren = item.children && item.children.length > 0;
+                  return (
                     <div key={item.title}>
                       <DirectoryTree
-                        showLine={{showLeafIcon: false}}
-                        switcherIcon={<DownOutlined/>}
+                        showLine={hasChildren ? { showLeafIcon: false } : false}
+                        switcherIcon={<DownOutlined />}
                         treeData={[item]}
                         titleRender={titleRender}
                         onSelect={onSelect}
                       />
-                      <Divider style={{margin: 0}}/>
+                      <Divider style={{ margin: '0' }} />
                     </div>
-                  )
-                  console.log(tmp)
-                  return tmp
-                }
-              )
-            }
+                  );
+                })}
+              </TabPane>
+            </Tabs>
           </PageContainer>
         </Col>
 
-        <Col span={4}/>
+        <Col span={4} />
       </Row>
     </div>
   );
