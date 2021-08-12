61e32d09bb4e8332402f2465b4766cee77dcf0cc
吴荣军 <wurongjun@corp.netease.com>
2021-08-12 15:54:59 +0800

    feat(compiler): step commit

--- src/models/groups.js
+++ src/models/groups.js
@@ -1,13 +1,60 @@
-import { useState, useCallback } from 'react';
-import { queryGroups } from '@/services/dashboard/groups';
+import { useCallback, useState } from 'react';
+
+const defaultGroups = [
+  {
+    title: 'music-cloud-native',
+    key: 'music-cloud-native',
+    children: [
+      {
+        title: 'app-deploy',
+        key: 'app-deploy',
+        children: [
+          {
+            title: 'dev',
+            key: 'dev',
+          },
+        ],
+      },
+      {
+        title: 'tomtest',
+        key: 'tomtest',
+      },
+    ],
+  },
+  {
+    title: 'common-code-block',
+    key: 'common-code-block',
+  },
+  {
+    title: 'music-pe',
+    key: 'music-pe',
+    children: [
+      {
+        title: 'app-deploy',
+        key: 'app-deploy1',
+        children: [
+          {
+            title: 'dev',
+            key: 'dev1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'cloudsre',
+    key: 'cloudsre',
+  },
+];
 
 export default function useAuthModel() {
   const [groups, setGroups] = useState([]);
 
   const queryGroup = useCallback(() => {
-    queryGroups().then(({ data }) => {
-      setGroups(data);
-    });
+    // queryGroups().then(({data}) => {
+    //   setGroups(data);
+    // });
+    setGroups(defaultGroups);
   }, []);
 
   return {
