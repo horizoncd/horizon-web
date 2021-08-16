5e150e40843a39eb06ecef7cb92959ed8652eed8
Rongjun Wu <rongjun0821@gmail.com>
2021-08-16 18:09:51 +0800

    feat(compiler): step commit

---
+++ src/pages/Details.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card } from 'antd';
+
+export default (props: any): React.ReactNode => {
+  console.log(props)
+
+  // 这里判断此pathname对应的是Group、App还是实例
+
+  return (
+    <PageContainer>
+      <Card>
+        <div>Details</div>
+      </Card>
+    </PageContainer>
+  );
+};
