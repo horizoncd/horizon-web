e3e9d1f7ad292325f6ce7479b593f282e9408492
吴荣军 <wurongjun@corp.netease.com>
2021-10-14 17:00:18 +0800

    1

--- src/app.tsx
+++ src/app.tsx
@@ -88,44 +88,6 @@ export async function getInitialState(): Promise<{
   };
 }
 
-/**
- * 异常处理程序
- 200: '服务器成功返回请求的数据。',
- 201: '新建或修改数据成功。',
- 202: '一个请求已经进入后台排队（异步任务）。',
- 204: '删除数据成功。',
- 400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
- 401: '用户没有权限（令牌、用户名、密码错误）。',
- 403: '用户得到授权，但是访问是被禁止的。',
- 404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
- 405: '请求方法不被允许。',
- 406: '请求的格式不可得。',
- 410: '请求的资源被永久删除，且不会再得到的。',
- 422: '当创建一个对象时，发生一个验证错误。',
- 500: '服务器发生错误，请检查服务器。',
- 502: '网关错误。',
- 503: '服务不可用，服务器暂时过载或维护。',
- 504: '网关超时。',
- //-----English
- 200: The server successfully returned the requested data. ',
- 201: New or modified data is successful. ',
- 202: A request has entered the background queue (asynchronous task). ',
- 204: Data deleted successfully. ',
- 400: 'There was an error in the request sent, and the server did not create or modify data. ',
- 401: The user does not have permission (token, username, password error). ',
- 403: The user is authorized, but access is forbidden. ',
- 404: The request sent was for a record that did not exist. ',
- 405: The request method is not allowed. ',
- 406: The requested format is not available. ',
- 410':
- 'The requested resource is permanently deleted and will no longer be available. ',
- 422: When creating an object, a validation error occurred. ',
- 500: An error occurred on the server, please check the server. ',
- 502: Gateway error. ',
- 503: The service is unavailable. ',
- 504: The gateway timed out. ',
- * @see https://beta-pro.ant.design/docs/request-cn
- */
 export const request: RequestConfig = {
   responseInterceptors: [
     (response) => {
@@ -200,20 +162,27 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
         </span>
       );
     },
-    menuDataRender: (menuData: MenuDataItem[]): MenuDataItem[] => {
-      if (pathnameInStaticRoutes() || !initialState) {
-        return menuData;
-      }
-      // 根据ResourceType决定菜单
-      const { type, fullPath } = initialState.resource;
-      switch (type) {
-        case 'group':
-          return loopMenuItem(formatGroupMenu(fullPath));
-        case 'application':
-          return loopMenuItem(formatApplicationMenu(fullPath));
-        default:
-          return menuData;
-      }
+    menu: {
+      // 每当 initialState?.currentUser?.userid 发生修改时重新执行 request
+      params: {
+        resource: initialState?.resource
+      },
+      request: async (params, defaultMenuData) => {
+        if (pathnameInStaticRoutes() || !initialState) {
+          return defaultMenuData;
+        }
+
+        // 根据ResourceType决定菜单
+        const { type, fullPath } = initialState.resource;
+        switch (type) {
+          case 'group':
+            return loopMenuItem(formatGroupMenu(fullPath));
+          case 'application':
+            return loopMenuItem(formatApplicationMenu(fullPath));
+          default:
+            return defaultMenuData;
+        }
+      },
     },
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
@@ -222,7 +191,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
 };
 
 function pathnameInStaticRoutes(): boolean {
-  const pathname = history.location.pathname;
+  const {pathname} = history.location;
   // handle url end with '/'
   let path = pathname;
   if (pathname.endsWith('/')) {
