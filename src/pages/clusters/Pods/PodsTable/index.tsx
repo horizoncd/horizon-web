23e4f363ba8baa80fa5f10e58b86af8984015749
yeka <wurongjun@corp.netease.com>
2022-06-28 19:51:43 +0800

    Feat: support delete pod
    Signed-off-by: yeka <wurongjun@corp.netease.com>

--- src/pages/clusters/Pods/PodsTable/index.tsx
+++ src/pages/clusters/Pods/PodsTable/index.tsx
@@ -36,7 +36,6 @@ import {
   PlusSquareTwoTone,
 } from "@ant-design/icons";
 import copy from "copy-to-clipboard";
-import {restart} from "@/services/clusters/clusters";
 
 const Search = withTrim(Input.Search);
 const pollingInterval = 5000;
@@ -335,7 +334,7 @@ export default (props: { data: CLUSTER.PodInTable[], cluster?: CLUSTER.Cluster }
           style={{marginLeft: '10px'}}
           onClick={() => {
             Modal.confirm({
-              title: '此操作将导致所有选中的Pod立刻被销毁，请注意评估流量风险！',
+              title:  `此操作将导致所有被选中的Pod（共${selectedPods.length}台）立刻被销毁，并创建出同等数量的新Pod，请注意评估流量风险！`,
               onOk() {
                 deletePods(cluster!.id, selectedPods.map(item => item.podName)).then(({data: d}) => {
                   hookAfterBatchOps("重新调度", d)
@@ -346,7 +345,7 @@ export default (props: { data: CLUSTER.PodInTable[], cluster?: CLUSTER.Cluster }
           disabled={!selectedPods.length || !RBAC.Permissions.deletePods.allowed}
         >
           <Tooltip
-            title={'销毁选中的Pod，重新创建同等数量的Pod'}
+            title={'销毁被选中的Pod，并创建出同等数量的新Pod'}
           >
             {formatMessage('reschedulePod', '销毁重建')}
           </Tooltip>
