4a458ad1991896f84e83a9c2717f2a473d178589
sph <shepenghui@corp.netease.com>
2022-06-29 17:54:06 +0800

    import gitlab/ui

--- src/pages/applications/Clusters/index.tsx
+++ src/pages/applications/Clusters/index.tsx
@@ -1,29 +1,35 @@
-import {Button, Input, Table, Tabs} from "antd";
+import { Button, Dropdown, Input, Menu, Select, Table, Tabs } from "antd";
 import PageWithBreadcrumb from '@/components/PageWithBreadcrumb'
-import {useState} from "react";
-import {history} from "@@/core/history";
-import {stringify} from "querystring";
-import {useIntl} from "@@/plugin-locale/localeExports";
-import {useModel} from "@@/plugin-model/useModel";
+import { useState } from "react";
+import { history } from "@@/core/history";
+import { stringify } from "querystring";
+import { useIntl } from "@@/plugin-locale/localeExports";
+import { useModel } from "@@/plugin-model/useModel";
 import styles from './index.less'
-import {useRequest} from "@@/plugin-request/request";
-import {queryEnvironments} from "@/services/environments/environments";
-import {queryClusters} from "@/services/clusters/clusters";
+import { useRequest } from "@@/plugin-request/request";
+import { queryEnvironments } from "@/services/environments/environments";
+import { queryClusters } from "@/services/clusters/clusters";
 import RBAC from '@/rbac'
 import Utils from '@/utils'
 import NoData from "@/components/NoData";
 import withTrim from "@/components/WithTrim";
+import { GlFilteredSearch } from '@gitlab/ui';
+import { applyVueInReact } from 'vuereact-combined'
 
-const {TabPane} = Tabs;
+
+const FilteredSearch = applyVueInReact(GlFilteredSearch)
+
+const { TabPane } = Tabs;
 const Search = withTrim(Input.Search);
+const { Option } = Select;
 
 export default (props: any) => {
-  const {query: q} = props.location;
-  const {environment = ''} = q
+  const { query: q } = props.location;
+  const { environment = '' } = q
 
   const intl = useIntl();
-  const {initialState} = useModel('@@initialState');
-  const {id, name: application, fullPath} = initialState!.resource;
+  const { initialState } = useModel('@@initialState');
+  const { id, name: application, fullPath } = initialState!.resource;
   const newCluster = `/applications${fullPath}/-/clusters/new`;
 
   const pageSize = 10;
@@ -71,25 +77,25 @@ export default (props: any) => {
   const [query, setQuery] = useState(0);
   const [env2DisplayName, setEnv2DisplayName] = useState<Map<string, string>>();
 
-  const {data: envs} = useRequest(queryEnvironments, {
+  const { data: envs } = useRequest(queryEnvironments, {
     onSuccess: () => {
       const e = new Map<string, string>();
       envs!.forEach(item => e.set(item.name, item.displayName))
       setEnv2DisplayName(e)
     }
   });
-  const {data: clusters} = useRequest(() => queryClusters(id, environment ? {
-      filter, pageNumber, pageSize, environment
-    } : {
-      filter, pageNumber, pageSize,
-    }
+  const { data: clusters } = useRequest(() => queryClusters(id, environment ? {
+    filter, pageNumber, pageSize, environment
+  } : {
+    filter, pageNumber, pageSize,
+  }
   ), {
     refreshDeps: [query, filter, environment, pageNumber],
     debounceInterval: 200,
   });
 
   const onChange = (e: any) => {
-    const {value} = e.target;
+    const { value } = e.target;
     setFilter(value);
   };
 
@@ -100,13 +106,31 @@ export default (props: any) => {
   const onSearch = () => {
     setQuery(prev => prev + 1)
   }
+
+  const onSearchChange = (value: string) => {
+
+  }
+
   const queryInput = (
     // @ts-ignore
-    <div><Search className={styles.antInputGroupWrapper} placeholder="Search" onPressEnter={onPressEnter} value={filter}
-                 onSearch={onSearch}
-                 onChange={onChange}
-    />
-      {
+    <div >
+      <FilteredSearch style={{height: '10px'}}>
+      </FilteredSearch>
+      {/* <Select style={{width: "150px"}} disabled={false} mode="tags" placeholder="support multiple values" /> */}
+      {/* <Select
+        style={{ width: '80px', marginRight: '5px' }}
+        defaultValue="filter"
+        onChange={onSearchChange}
+      >
+        <Option value='filter'>名称</Option>
+        <Option value='label'>标签</Option>
+      </Select>
+
+      <Search className={styles.antInputGroupWrapper} placeholder="Search" onPressEnter={onPressEnter} value={filter}
+        onSearch={onSearch}
+        onChange={onChange}
+      /> */}
+      {/* {
         <Button
           type="primary"
           disabled={!RBAC.Permissions.createCluster.allowedEnv(environment)}
@@ -121,14 +145,14 @@ export default (props: any) => {
             });
           }}
         >
-          {intl.formatMessage({id: 'pages.groups.New cluster'})}
+          {intl.formatMessage({ id: 'pages.groups.New cluster' })}
         </Button>
-      }
+      } */}
     </div>
   )
 
   const data = clusters?.items.map(item => {
-    const {name, scope, template, updatedAt, createdAt} = item
+    const { name, scope, template, updatedAt, createdAt } = item
     return {
       key: name,
       name: name,
@@ -184,13 +208,13 @@ export default (props: any) => {
   return (
     <PageWithBreadcrumb>
       <Tabs activeKey={environment} size={'large'} tabBarExtraContent={queryInput} onChange={tabOnChange}
-            animated={false}>
+        animated={false}>
         <TabPane tab={'所有'} key={''}>
           {table}
         </TabPane>
         {
           envs?.map(item => {
-            const {name, displayName} = item
+            const { name, displayName } = item
             return <TabPane tab={displayName} key={name}>
               {table}
             </TabPane>
