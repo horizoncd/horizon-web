723db43aaf226cb01a2f971596f1a87ebfed991a
zhuxu04 <zhuxu04@corp.netease.com>
2022-12-01 20:35:55 +0800

    fix: rebase

--- src/components/HeaderContent/index.tsx
+++ src/components/HeaderContent/index.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import { history, useIntl, useModel } from 'umi';
+import { useIntl, useModel } from 'umi';
 import { Menu } from 'antd';
 import { DownOutlined } from '@ant-design/icons/lib';
 import { BoldText } from '@/components/Widget';
@@ -18,50 +17,45 @@ const GlobalHeaderContent: React.FC = () => {
       selectable={false}
     >
       <Menu.Item key="1">
-        <a onClick={() => history.push('/dashboard/clusters')}>
+        <a href="/dashboard/clusters">
           <BoldText>{intl.formatMessage({ id: 'pages.header.clusters' })}</BoldText>
         </a>
       </Menu.Item>
       <Menu.Item key="2">
-        <a onClick={() => history.push('/dashboard/applications')}>
+        <a href="/dashboard/applications">
           <BoldText>{intl.formatMessage({ id: 'pages.header.applications' })}</BoldText>
         </a>
       </Menu.Item>
       <Menu.Item key="3">
-        <a onClick={() => history.push('/dashboard/groups')}>
+        <a href="/dashboard/groups">
           <BoldText>{intl.formatMessage({ id: 'pages.header.groups' })}</BoldText>
         </a>
       </Menu.Item>
       <Menu.Item key="7">
-        <a onClick={() => history.push('/templates')}>
+        <a href="/templates">
           <BoldText>{intl.formatMessage({ id: 'pages.header.templates' })}</BoldText>
         </a>
       </Menu.Item>
-      <SubMenu
-        key="4"
-        title={(
-          <span style={{ fontWeight: 'bold', color: 'rgba(255, 255, 255, 0.65)' }}>
-            {intl.formatMessage({ id: 'pages.common.more' })}
-            {' '}
-            <DownOutlined style={{ fontSize: 'x-small', color: 'rgba(255, 255, 255, 0.65)' }} />
-          </span>
-        )}
-      >
-        <Menu.Item key="5">
-          <a href="/slo">
-            <BoldText>{intl.formatMessage({ id: 'pages.header.slo' })}</BoldText>
-          </a>
-        </Menu.Item>
-        {
-            initialState?.currentUser?.isAdmin && (
+      {
+        initialState?.currentUser?.isAdmin && (
+          <SubMenu
+            key="4"
+            title={(
+              <span style={{ fontWeight: 'bold', color: 'rgba(255, 255, 255, 0.65)' }}>
+                {intl.formatMessage({ id: 'pages.common.more' })}
+                {' '}
+                <DownOutlined style={{ fontSize: 'x-small', color: 'rgba(255, 255, 255, 0.65)' }} />
+              </span>
+            )}
+          >
             <Menu.Item key="6">
               <a href="/admin">
                 <BoldText>{intl.formatMessage({ id: 'pages.header.admin' })}</BoldText>
               </a>
             </Menu.Item>
-            )
-          }
-      </SubMenu>
+          </SubMenu>
+        )
+      }
     </Menu>
   );
 };
