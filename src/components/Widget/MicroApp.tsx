0ebe55a6380b8fcc5d6659bc2757c51569097e28
sph <shepenghui@corp.netease.com>
2023-01-06 23:06:24 +0800

    fix: support to create v2 application

--- src/components/Widget/MicroApp.tsx
+++ src/components/Widget/MicroApp.tsx
@@ -1,11 +1,11 @@
-import { PropsWithChildren, useState } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import { MicroApp as UmiMicroApp } from 'umi';
 import { Props as MicroAppProps } from '../../.umi/plugin-qiankun/MicroApp';
 import { qiankun } from '@/app';
 
-function WithCheck<Props extends MicroAppProps>(WrappedComponent: React.ComponentType<Props>): React.FC<Props> {
+function WithCheck<Props extends MicroAppProps & { defaultNode?: React.ReactElement }>(WrappedComponent: React.ComponentType<Props>): React.FC<Props> {
   return function Inner(props: PropsWithChildren<Props>): React.ReactElement {
-    const { name } = props;
+    const { name, defaultNode = <div /> } = props;
     const [ifRender, setIfRender] = useState(false);
     qiankun.then(({ apps }) => {
       apps.forEach((app: SYSTEM.MicroApp) => {
@@ -22,7 +22,7 @@ function WithCheck<Props extends MicroAppProps>(WrappedComponent: React.Componen
         <WrappedComponent {...props} />
         {' '}
       </div>
-    ) : <div />;
+    ) : defaultNode;
   };
 }
 
