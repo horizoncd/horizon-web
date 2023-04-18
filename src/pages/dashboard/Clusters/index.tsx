c378f97454869a80eb0a8f66ccca7abbe8e312a6
Yaochen Ling <4closetool3@gmail.com>
2023-04-18 10:42:08 +0800

    Update index.tsx

--- src/pages/dashboard/Clusters/index.tsx
+++ src/pages/dashboard/Clusters/index.tsx
@@ -189,7 +189,7 @@ function Clusters(props: ClustersProps) {
       [QueryEnv]: qEnv = '',
       [QueryRelease]: qRelease = '',
       [QueryTemplate]: qTemplate = '',
-      [QueryMode]: qMode = Mode.Own,
+      [QueryMode]: qMode = Mode.All,
       [QueryRegion]: qRegion = '',
     } = location.query ?? {};
 
